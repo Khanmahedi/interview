bf71d897e4dec1849c369d227eec8729e054410e
khanmahedi <md.mahedihasan1993@gmail.com>
2024-03-26 12:56:47 +0600

    Static variable example code

--- Static_variable_in_class.cpp
+++ Static_variable_in_class.cpp
@@ -1,1 +1,31 @@
-#include<bits/stdc++
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Employe{
+    int id ;   // by default private
+    static int count ;
+public:
+     void setId(){ 
+        count++ ;  
+        cin >> id ;
+        return ;
+    }
+    void getId(){
+        cout << "This is Employe id : " << id << endl ;
+        cout << "Employe number " << count << endl ;
+        return ;
+    }
+
+};
+
+int Employe :: count ;
+
+int main(){
+    Employe one ;
+    one.setId() ;
+    one.getId() ;
+    Employe two ;
+    two.setId() ;
+    two.getId() ;
+}
